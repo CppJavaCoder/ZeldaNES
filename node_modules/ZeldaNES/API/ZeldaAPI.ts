38a57c3a10ac8e3c093e28901dec1bd4373128d7
Java <64042936+CppJavaCoder@users.noreply.github.com>
2021-01-31 19:42:39 -0600

    Delete node_modules directory

--- node_modules/ZeldaNES/API/ZeldaAPI.ts
+++
@@ -1,9 +0,0 @@
-import { ICore, IModLoaderAPI } from 'modloader64_api/IModLoaderAPI';
-
-export interface IGlobalContext {
-}
-
-export interface ZeldaCore extends ICore {
-    global: IGlobalContext;
-
-}
